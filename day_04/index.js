9a4bc2f5912c32ca32274960442a3696b8c709eb
Nhi Tran <nhitran@nhis-mbp-2.myfiosgateway.com>
2020-12-06 23:18:49 -0500

    4.2

--- day_04/index.js
+++ day_04/index.js
@@ -1,10 +1,43 @@
 const fs = require("fs");
 
-function validPassport(passport, requiredKeys = []) {
-    if (!passport) return 0
-    return requiredKeys.every(requiredKey => 
-        Object.keys(passport).includes(requiredKey)) ? 1 : 0
+const inRange = (min, max) => (value) => {
+    return value >= min && value <= max;
 }
+
+const inHeightRange = rangeRule => str => {
+    if (!str.match(/(cm|in)$/)) return false;
+
+    // must start with nubmers
+    const unit = str.slice(str.length - 2);
+    const value = str.substr(0, str.length - 2)
+    if (!unit && !value) return false
+
+    return inRange(rangeRule[unit].min, rangeRule[unit].max)(value)
+}
+
+const eyeRule = eyeColors => value => eyeColors.includes(value)
+
+const hairRule = regexExp => str => !!str.match(regexExp);
+
+const pidRule = regexExp => str => !!str.match(regexExp);
+
+function validPassport(passport, passportKeys) {
+    if (!passport) return 0
+
+    const requiredKeys = Object.keys(passportFields).filter(key => passportFields[key])
+
+    if(!requiredKeys.every(requiredKey =>  Object.keys(passport).includes(requiredKey))) return 0;
+
+
+    return passportKeys['byr'](passport['byr']) &&
+    passportKeys['iyr'](passport['iyr']) &&
+    passportKeys['eyr'](passport['eyr']) &&
+    passportKeys['hgt'](passport['hgt']) &&
+    passportKeys['hcl'](passport['hcl']) &&
+    passportKeys['ecl'](passport['ecl']) &&
+    passportKeys['pid'](passport['pid'])
+}
+
 
 function generatePassport(str, passport = {}) {
     if (!str.length) {
@@ -29,13 +62,13 @@ function generatePassport(str, passport = {}) {
 }
 
 const passportFields = {
-    byr: true,
-    iyr: true,
-    eyr: true,
-    hgt: true,
-    hcl: true,
-    ecl: true,
-    pid: true,
+    byr: inRange(1920, 2002),
+    iyr: inRange(2010, 2020),
+    eyr: inRange(2020, 2030),
+    hgt: inHeightRange({ "cm": {min: 150, max: 193}, "in": {min: 59, max: 76 }}),
+    hcl: hairRule(/^#[0-9a-f]{6}/),
+    ecl: eyeRule(['amb, blu',' brn', 'gry', 'hzl', 'oth']),
+    pid: pidRule(/[0-9]{9}/),
     cid: false
 }
 
@@ -44,30 +77,38 @@ const requiredPassportFields = Object.keys(passportFields).filter(key => passpor
 fs.readFile("input.txt", "utf8", function (err, data) {
     const lines = data.split(/\r?\n/);
 
+    console.log('%clines', 'color:pink', lines);
     let stack = []
     let passports = []
     let str = "";
 
-    lines.map(line => {
-        if (line && !line == " ") {
-            stack.push(line)
-        } else {
-            while (stack.length) {
-                if (!str.length) str += stack.pop();
-                else {
-                    str += " " + stack.pop()
-                }
-            }
-            passports.push(str);
-            str = ""
+    let i = 0;
+    while ( i < lines.length) {
+        while (lines[i] !== " " && lines[i]) {
+            stack.push(lines[i])
+            ++i;
         }
+        while (stack.length) {
+            if (!str.length) str += stack.pop();
+            else {
+                str += " " + stack.pop()
+            }
+        }
+        passports.push(str);
+        str = ""
+        ++i;
+    }
 
-    })
+    console.log('%cpass', 'color:pink', passports);
+
+
     const count = passports.reduce((acc, curr, idx) => {
         const temp = generatePassport(curr);
-        acc += validPassport(temp, requiredPassportFields);
+        acc += validPassport(temp, passportFields)
         return acc;
     }, 0)
+
+
 
     console.log('adventCode Result', count);
 
@@ -75,6 +116,10 @@ fs.readFile("input.txt", "utf8", function (err, data) {
   
 
 module.exports = {
+    eyeRule,
     generatePassport,
+    hairRule,
+    pidRule,
+    inRange,
     validPassport
 }
--- day_04/index.test.js
+++ day_04/index.test.js
@@ -1,4 +1,4 @@
-const { generatePassport, validPassport } = require('./index')
+const { eyeRule, hairRule, inRange, generatePassport, pidRule, validPassport } = require('./index')
 
 describe('validPasspport', () => {
     test('returns 0 if no passport', () => {
@@ -14,15 +14,15 @@ describe('validPasspport', () => {
         const requiredKeys = ['a', 'b']
         expect(validPassport(passport, requiredKeys)).toEqual(0)
     })
-    test('returns 1 if passport does not include all required keys', () => {
-        const passport = {
-            a: 1,
-            b: 2,
-            c: 3
-        }
-        const requiredKeys = ['a', 'b']
-        expect(validPassport(passport, requiredKeys)).toEqual(1)
-    })
+    // test('returns 1 if all required keys habve a valid value', () => {
+    //     const passport = {
+    //         a: 1,
+    //         b: 2,
+    //         c: 3
+    //     }
+    //     const requiredKeys = ['a', 'b']
+    //     expect(validPassport(passport, requiredKeys)).toEqual(1)
+    // })
 })
 
 describe('generatePassport', () => {
@@ -34,7 +34,47 @@ describe('generatePassport', () => {
         const str = "abc:1 def:2 ghi:3";
         const passport = {}
 
-        console.log('%cgeneratePassport(str, passport, 0)', 'color:pink', generatePassport(str, passport, 0));
-        expect(generatePassport(str, passport)).toEqual({ 'abc:': '1', 'def:': '2', 'ghi:': '3' })
+        expect(generatePassport(str, passport)).toEqual(
+            { abc: '1', def: '2', ghi: '3' })
+    })
+})
+
+describe('rules', () => {
+    test('inRange returns false if below minimum value', () => {
+        expect(inRange(1920, 2002)(1910)).toEqual(false)
+    })
+    test('inRange returns false if above max value', () => {
+        expect(inRange(1920, 2002)(2003)).toEqual(false)
+    })
+    test('inRange returns true if equal to min value', () => {
+        expect(inRange(1920, 2002)(1920)).toEqual(true)
+    })
+    test('inRange returns true if equal to max value', () => {
+        expect(inRange(1920, 2002)(2002)).toEqual(true)
+    })
+    test('inRange returns true if value within range of min and max', () => {
+        expect(inRange(1920, 2002)(2002)).toEqual(true)
+    })
+    test('hairRule should return true if value begins with # and followed by exactly six characters 0-9 or a-f', () => {
+        expect(hairRule(/^#[0-9a-f]{6}/)("#123456")).toEqual(true)
+        expect(hairRule(/^#[0-9a-f]{6}/)("#123abc")).toEqual(true)
+        expect(hairRule(/^#[0-9a-f]{6}/)("#abc123")).toEqual(true)
+        expect(hairRule(/^#[0-9a-f]{6}/)("#def789")).toEqual(true)
+    })
+    test('hairRule should return false if value does not begin with # and followed by exactly six characters 0-9 or a-f', () => {
+        expect(hairRule(/^#[0-9a-f]{6}/)("?123456")).toEqual(false)
+        expect(hairRule(/^#[0-9a-f]{6}/)("#012")).toEqual(false)
+        expect(hairRule(/^#[0-9a-f]{6}/)("#gab123")).toEqual(false)
+    })
+    test('eyeRule should return false if value is not one of the listed eye colors', () => {
+        expect(eyeRule(['amb', 'blu',' brn', 'gry', 'hzl', 'oth'])('abc')).toEqual(false)
+    })
+    test('eyeRule should return true if value is one of the listed eye colors', () => {
+        expect(eyeRule(['amb', 'blu',' brn', 'gry', 'hzl', 'oth'])('hzl')).toEqual(true)
+        expect(eyeRule(['amb', 'blu',' brn', 'gry', 'hzl', 'oth'])('blu')).toEqual(true)
+    })
+    test('pid rule should return false if string does not have 9 digits', () => {
+        expect(pidRule(/[0-9]{9}/)("000000001")).toEqual(true)
+        expect(pidRule(/[0-9]{9}/)("000")).toEqual(false)
     })
 })
